9f5fe400543d98987dff8ebe361006b500b39d0c
Corey Nance <coreynance83@gmail.com>
2021-08-13 22:20:20 -0400

    fixes for background image in header and resume download in Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Header.css';
-
+import backgroundPic from '../Assets/pexels-pixabay-209207.png';
+// import myPic from '../Assets/images/myImage.jpg'
 // By importing the Header.css file, it is added to the DOM whenever this component loads
 
 // We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
@@ -8,12 +9,11 @@ import '../styles/Header.css';
 // On a style object, we camelCase all property names, and put all of the values in quotes
 // Non quoted values default to "pixels", e.g. height, margin, padding
 
-
 // We use JSX curly braces to evaluate the style object
 
 function Header() {
   return (
-    <header>
+    <header style={{ backgroundImage: `url(${backgroundPic})` }}>
     <h1>Corey Nance </h1>
   </header>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Navbar.css';
+import resume from '../Assets/Coreys Resume 5.pdf'
 
 // By importing the Navbar.css file, it is added to the DOM whenever this component loads
 
@@ -25,7 +26,7 @@ function Navbar() {
                 <li><a href = "#aboutMe">About Me</a></li>
                 <li><a href = "#work">Work</a></li>
                 <li><a href = "#contactMe">Contact Me</a></li>
-                <li><a href = "Assets\Coreys Resume 5.pdf" download target= "_blank">Resume</a></li>
+                <li><a href = {`${resume}`} download target= "_blank">Resume</a></li>
             </h2>
         </ul>
     </nav>
